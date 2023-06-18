066b91f30fc4072b99337bf49ccf7dc9c3c30d5f
adotrdot <a.aryanta.s@gmail.com>
2023-06-18 22:31:34 +0700

    prototipe 0.1 (add mekanisme zoom & drag camera)

--- Level.gd
+++ Level.gd
@@ -3,12 +3,7 @@ extends Node2D
 var can_place = true
 @onready var tilemap = $TileMap
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if can_place:
 		if Input.is_action_pressed("mb_left"):
@@ -20,7 +15,7 @@ func place_tile():
 	var mousepos : Vector2i = get_local_mouse_position()
 	var tilepos = tilemap.local_to_map(mousepos)
 	tilemap.set_cells_terrain_connect(0, [tilepos], 0, 0)
-	pass
+	
 	
 func remove_tile():
 	var mousepos : Vector2i = get_local_mouse_position()
@@ -30,4 +25,3 @@ func remove_tile():
 		if tilemap.get_cell_source_id(0, tile) != -1:
 			tilemap.erase_cell(0, tile)
 			tilemap.set_cells_terrain_connect(0, [tile], 0, 0)
-	pass
---
+++ cam_container.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+var is_panning = false
+@onready var camera = $Camera2D
+@export var cam_spd = 10
+
+func _ready():
+	camera.make_current()
+
+
+func _process(delta):
+	is_panning = Input.is_action_pressed("mb_mid")
+
+
+func _unhandled_input(event):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			if event.button_index == MOUSE_BUTTON_WHEEL_UP and camera.zoom < Vector2(2,2):
+				camera.zoom += Vector2(0.1,0.1)
+			if event.button_index == MOUSE_BUTTON_WHEEL_DOWN and camera.zoom > Vector2(0.5,0.5):
+				camera.zoom -= Vector2(0.1,0.1)
+	if(event is InputEventMouseMotion):
+		if(is_panning):
+			self.global_position -= event.relative
